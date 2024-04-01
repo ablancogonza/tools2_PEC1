46f85142b4448c60025c4dfd20633c38a8acf4b0
abg <softabg@gmail.com>
2024-04-01 11:34:11 +0200

    corregido menu

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -10,11 +10,21 @@
  * Write any other JavaScript below
  */
 
-window.addEventListener("click", function swapMenu() {
-  const navbar = this.document.getElementById("navbar__list");
+var icon = document.getElementById("icon");
+
+icon.addEventListener("click", function swapMenu() {
+  const navbar = document.getElementById("navbar__list");
   if (navbar.className === "navbar__list") {
     navbar.className += " navbar__list--visible";
   } else {
     navbar.className = "navbar__list";
   }
 });
+
+var navbar_list = document.getElementById("navbar__list");
+for (const child of navbar_list.children) {
+  child.addEventListener("click", function() {
+    const navbar = document.getElementById("navbar__list");
+    navbar.classList.remove("navbar__list--visible");
+  });
+};
